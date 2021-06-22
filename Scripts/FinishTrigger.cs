c5cb4b537181aac52946c7ebe8b27c7868c2755c
Avi Gupta <86228752+mirrorffs@users.noreply.github.com>
2021-06-22 14:43:44 +0530

    Add files via upload

---
+++ Scripts/FinishTrigger.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+
+	public GameObject LapCompleteTrig;
+	public GameObject HalfLapTrig;
+
+	void OnTriggerEnter()
+	{
+		LapCompleteTrig.SetActive(true);
+		HalfLapTrig.SetActive(false);
+	}
+}
